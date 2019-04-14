c8dee20770b95becf6b23440c929e51d01146fa4
Daniel Szlauer <dszlauer@Daniels-MacBook-Pro.local>
2019-04-14 10:38:28 -0700

    final changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,46 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { Route, Switch, NavLink } from "react-router-dom";
+import Home from "./Home/Home";
+import About from "./About/About";
+import People from "./People/People";
+import FAQ from "./About/FAQ/FAQ";
+import Company from "./About/Company/Company";
+import Character from "./Character/Character";
 
 class App extends Component {
   render() {
     return (
       <div>
-        This is App.js
+        <header>
+          <NavLink activeClassName="active" exact to="/">
+            Home
+          </NavLink>
+          <NavLink activeClassName="active" to="/about">
+            About
+          </NavLink>
+          <NavLink activeClassName="active" to="/People">
+            People
+          </NavLink>
+        </header>
+        {/* if we did not have exact it would show both Home and about at the same time */}
+        {/* "/" means home directory */}
+        <Switch>
+          <Route
+            path="/about"
+            render={() => {
+              return (
+                <About>
+                  <Route path="/about/faq" component={FAQ} />
+                  <Route path="/about/company" component={Company} />
+                </About>
+              );
+            }}
+          />
+          <Route path="/people/:id" component={Character} />
+          <Route path="/people" component={People} />
+          {/* Moved home path down because Switch will match exactly, so onece it hits "/" it stops. */}
+          <Route path="/" component={Home} />
+        </Switch>
       </div>
     );
   }
